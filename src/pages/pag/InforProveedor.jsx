c12d9cebd732f662d4a05e28c6321dfcd25d81cd
Danilo Montecel <daniloomarmontecel@gmail.com>
2024-07-17 17:58:33 -0500

    cambios agrege boton enable y disabled

--- src/pages/pag/InforProveedor.jsx
+++ src/pages/pag/InforProveedor.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import '../style/modal.css'
 import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCheck } from '@fortawesome/free-solid-svg-icons'; // ícono de usuario habilitado
+import { faUserTimes } from '@fortawesome/free-solid-svg-icons';
 //import Modal from 'react-bootstrap/Modal';
 //import { Stack } from "react-bootstrap";
 const InforProveedor = ({ item, deleteApi, setIsOpen, setEditProv }) => {
+  const [suplierHabilitado, setSuplierHabiltado]= useState(true)
   const [isModal, setIsModal] = useState(false);
   const handleDelete = () =>{
     deleteApi("/proveedor", item.id);  
@@ -17,13 +21,15 @@ const InforProveedor = ({ item, deleteApi, setIsOpen, setEditProv }) => {
  // const handleClose = () =>{
   //   setIsModal(false);
  // }
-
+   const toggleHabilitarSupliers =() =>{
+    setSuplierHabiltado(!suplierHabilitado)
+   }
   return (
     <>
       
 
 <div className='btn1'> 
-        <Button className='delete' onClick={()=>setIsModal(true)}><ion-icon name="trash-outline"></ion-icon></Button>
+        <Button className='delete' disabled={!suplierHabilitado} onClick={()=>setIsModal(true)}><ion-icon name="trash-outline"></ion-icon></Button>
         {
             isModal && (
                 
@@ -34,14 +40,20 @@ const InforProveedor = ({ item, deleteApi, setIsOpen, setEditProv }) => {
                     <Button className='delete_user1' onClick={()=>setIsModal(false)}>Close</Button>
                     </div>
                 </form>
-            
-            
-
             )
-            
         }
-       
-       <Button className='edit' onClick={handleEdit}><ion-icon name="create-outline"></ion-icon></Button>
+       <Button className='edit' disabled={!suplierHabilitado} onClick={handleEdit}><ion-icon name="create-outline"></ion-icon></Button>
+       <Button className='enable' onClick={toggleHabilitarSupliers}>
+        {suplierHabilitado ? (
+          <>
+            <FontAwesomeIcon icon={faUserCheck} />
+          </>
+        ) : (
+          <>
+            <FontAwesomeIcon icon={faUserTimes} />
+          </>
+        )}
+      </Button>
         </div> 
     </>
 
